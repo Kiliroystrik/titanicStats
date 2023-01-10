646949358d53e4f4333206411c86c9b553ada3c3
Kiliroy <borges.mathieu@gmail.com>
2023-01-10 09:33:39 +0100

    Create passenger Model

---
+++ src/app/Models/PassengerModel.ts
@@ -0,0 +1,14 @@
+export class Passenger {
+
+    public PassengerId!: string;
+    public Survived!: string;
+    public Pclass!: string;
+    public Name!: string;
+    public Sex!: string;
+    public Age!: string;
+    public SibSp!: string;
+    public Parch!: string;
+    public Ticket!: string;
+    public Fare!: string;
+    public Embarked!: string;
+}
