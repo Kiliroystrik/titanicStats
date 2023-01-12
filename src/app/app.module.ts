d8d21900ffd0aef875ed127ef2137441778bdbc8
Kiliroy <borges.mathieu@gmail.com>
2023-01-12 13:44:46 +0100

    add dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
+import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 import { MenuComponent } from './menu/menu.component';
 import { HttpClientModule } from '@angular/common/http'
+import { GuardService } from './services/guard.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { NgChartsModule } from 'ng2-charts';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { PassengersListComponent } from './passengers/passengers-list/passengers-list.component';
+import { PassengersGraphComponent } from './passengers/passengers-graph/passengers-graph.component';
+
+
 
 
 @NgModule({
@@ -15,15 +28,26 @@ import { HttpClientModule } from '@angular/common/http'
     AppComponent,
     LoginComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    PassengersListComponent,
+    PassengersGraphComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    NgChartsModule,
+    MatCheckboxModule,
+    MatRadioModule
   ],
-  providers: [],
+  providers: [GuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
